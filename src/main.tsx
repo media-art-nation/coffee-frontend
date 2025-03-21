9b8788f04095f8ad13572f0aa7101e03805da7cc
eunhee <6810779s@naver.com>
2025-03-21 19:15:20 +0900

    [feat] 공통 컴포넌트 palette 및 theme 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+
+import { createRoot } from 'react-dom/client';
+
+import { ThemeProvider } from '@mui/material';
+
+import { theme } from '@/themes/index.ts';
+
+import App from './App.tsx';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StrictMode>
+        <ThemeProvider theme={theme}>
+            <App />
+        </ThemeProvider>
+    </StrictMode>
+);
---
+++ src/themes/palette.ts
@@ -0,0 +1,90 @@
+import { Color, PaletteOptions } from '@mui/material';
+
+interface CustomColor extends Omit<Color, 'A100' | 'A200' | 'A400' | 'A700'> {
+    main: string;
+}
+
+interface CustomPaletteOptions extends PaletteOptions {
+    blue: CustomColor;
+    grey: CustomColor;
+    success: CustomColor;
+    error: CustomColor;
+    yellow: CustomColor;
+    divider: string;
+    // TODO: text, common, action color 확정 시 타입 재정의 2024.04.01 kbc
+    text: { primary: string };
+    common: { black: string; white: string };
+    action: { hover: string; focus: string; selected: string };
+}
+
+export const palette: CustomPaletteOptions = {
+    blue: {
+        main: '#2196f3', // 500
+        50: '#e3f2fd',
+        100: '#bbdefb',
+        200: '#90caf9',
+        300: '#64b5f6',
+        400: '#42a5f5',
+        500: '#2196f3',
+        600: '#1e88e5',
+        700: '#1976d2',
+        800: '#1565c0',
+        900: '#0d47a1',
+    },
+    grey: {
+        main: '9e9e9e',
+        50: '#fafafa',
+        100: '#f5f5f5',
+        200: '#eeeeee',
+        300: '#e0e0e0',
+        400: '#bdbdbd',
+        500: '#9e9e9e',
+        600: '#757575',
+        700: '#616161',
+        800: '#424242',
+        900: '#212121',
+    },
+    success: {
+        main: '#0E9F6E', // 500
+        50: '#F3FAF7',
+        100: '#DEF7EC',
+        200: '#BCF0DA',
+        300: '#84E1BC',
+        400: '#31C48D',
+        500: '#0E9F6E',
+        600: '#057A55',
+        700: '#046C4E',
+        800: '#03543F',
+        900: '#014737',
+    },
+    error: {
+        main: '#F05252', // 500
+        50: '#FDF2F2',
+        100: '#FDE8E8',
+        200: '#FBD5D5',
+        300: '#F8B4B4',
+        400: '#F98080',
+        500: '#F05252',
+        600: '#E02424',
+        700: '#C81E1E',
+        800: '#9B1C1C',
+        900: '#771D1D',
+    },
+    yellow: {
+        main: '#ffeb3b', // 500
+        50: '#fffde7',
+        100: '#fff9c4',
+        200: '#fff59d',
+        300: '#fff176',
+        400: '#ffee58',
+        500: '#ffeb3b',
+        600: '#fdd835',
+        700: '#fbc02d',
+        800: '#f9a825',
+        900: '#f57f17',
+    },
+    divider: '#E5E7EB',
+    text: { primary: '#111928' }, // grey 900
+    common: { black: '#000000', white: '#FFFFFF' },
+    action: { hover: '#0000000A', focus: '#0000000A', selected: '#EBF5FF' }, // TODO: disabled, disabledBackground, disabledOpacity color 2024.04.01 kbc
+};
