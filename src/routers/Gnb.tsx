b2408ecf68df0f8d6442fad8e94612f35c6d7fd6
yuna872 <yunae426@gmail.com>
2025-03-24 01:29:47 +0900

    [feat] gnb 컴포넌트 디자인 반영

--- src/routers/Gnb.tsx
+++ src/routers/Gnb.tsx
@@ -1,5 +1,38 @@
+import { Stack, Typography } from '@mui/material';
+
+import { palette } from '@/themes';
+
+export const GNB_HEIGHT = 60;
 const Gnb = () => {
-    return <>header</>;
+    const isLogin = true;
+    const handleClickLogout = () => {};
+
+    return (
+        <Stack
+            sx={{
+                '& .MuiTypography-root': {
+                    color: palette.common.white,
+                },
+                'backgroundColor': palette.blue[500],
+                'padding': '0 20px',
+                'flexDirection': 'row',
+                'justifyContent': 'space-between',
+                'alignItems': 'center',
+                'height': GNB_HEIGHT,
+            }}
+        >
+            <Typography sx={{ fontSize: '22px', fontWeight: '600' }}>테즈로스팅</Typography>
+            {isLogin && (
+                <Stack sx={{ flexDirection: 'row', gap: '15px' }}>
+                    <Typography>{`{ 아이디 }`}</Typography>
+                    <Typography sx={{ cursor: 'pointer' }}>내 정보 수정</Typography>
+                    <Typography sx={{ cursor: 'pointer' }} onClick={handleClickLogout}>
+                        로그아웃
+                    </Typography>
+                </Stack>
+            )}
+        </Stack>
+    );
 };
 
 export default Gnb;
