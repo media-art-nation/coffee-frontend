e8cef9a678971d835ffdbd63f04c388e0bce49f1
yuna872 <yunae426@gmail.com>
2025-04-13 19:46:56 +0900

    [style] title padding 수정

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -11,7 +11,7 @@ const Title = ({ title, children }: TitleProps) => {
             sx={{
                 flexDirection: 'row',
                 justifyContent: 'space-between',
-                padding: '32px',
+                padding: '24px 32px',
                 width: '100%',
             }}
         >
