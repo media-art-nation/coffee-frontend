2a619193771c189a31ec8959843441e7a77c4af3
eunhee <6810779s@naver.com>
2025-04-14 19:37:43 +0900

    [feat] 농부 목록 화면 레이아웃 구현

--- src/pages/Farmers/FarmerList/index.tsx
+++ src/pages/Farmers/FarmerList/index.tsx
@@ -1,7 +1,41 @@
-import { Stack } from '@mui/material';
+import { Stack, TableCell, TableRow } from '@mui/material';
 
+import PageLayout from '@/components/PageLayout';
+import Table from '@/components/Table';
+import Title from '@/components/Title';
+
+interface TDummy {
+    name: string;
+    villageHeader: string;
+    section: string;
+}
 const FarmerList = () => {
-    return <Stack sx={{ width: '100%' }}>농부 목록</Stack>;
+    const dummy = [
+        { name: 'dummy', villageHeader: 'dummy', section: 'dummy' },
+        { name: 'dummy', villageHeader: 'dummy', section: 'dummy' },
+        { name: 'dummy', villageHeader: 'dummy', section: 'dummy' },
+    ];
+    const renderRow = (row: TDummy) => {
+        return (
+            <TableRow>
+                <TableCell>{row.name}</TableCell>
+                <TableCell>{row.villageHeader}</TableCell>
+                <TableCell>{row.section}</TableCell>
+            </TableRow>
+        );
+    };
+    return (
+        <Stack>
+            <Title title="농부 목록" />
+            <PageLayout>
+                <Table
+                    headData={['이름', '관할 면장', '섹션명']}
+                    bodyData={dummy}
+                    renderRow={renderRow}
+                />
+            </PageLayout>
+        </Stack>
+    );
 };
 
 export default FarmerList;
