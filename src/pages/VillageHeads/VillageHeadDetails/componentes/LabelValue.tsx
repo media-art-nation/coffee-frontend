e765c271daacc2cd6452f91cc7c806daad06c7f7
eunhee <6810779s@naver.com>
2025-04-15 21:33:12 +0900

    [feat] 면장 상세정보 페이지 레이아웃 구현

---
+++ src/pages/VillageHeads/VillageHeadDetails/componentes/LabelValue.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { Stack, Typography } from '@mui/material';
+
+interface LabelValueProps {
+    label: string;
+    value: string;
+}
+const LabelValue: React.FC<LabelValueProps> = ({ label, value }) => {
+    return (
+        <Stack direction={'row'} sx={{ height: '40px' }}>
+            <Typography variant="title/medium" sx={{ width: '120px' }}>
+                {label}
+            </Typography>
+            <Typography variant="title/medium">{value}</Typography>
+        </Stack>
+    );
+};
+
+export default LabelValue;
