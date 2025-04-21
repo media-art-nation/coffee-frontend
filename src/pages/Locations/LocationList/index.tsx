cb66b0390aeefeb180aa51e57b96db1f98ba2066
eunhee <6810779s@naver.com>
2025-04-21 16:41:56 +0900

    [feat] 목록 관련 레이아웃 화면 구현 완료

--- src/pages/Locations/LocationList/index.tsx
+++ src/pages/Locations/LocationList/index.tsx
@@ -1,7 +1,36 @@
-import { Stack } from '@mui/material';
+import { Stack, TableCell, TableRow } from '@mui/material';
 
+import PageLayout from '@/components/PageLayout';
+import Table from '@/components/Table';
+import Title from '@/components/Title';
+
+interface TDummy {
+    id: number;
+    area: string;
+    section: string;
+}
 const LocationList = () => {
-    return <Stack sx={{ width: '100%' }}>지역 및 섹션 목록</Stack>;
+    const dummy: TDummy[] = [
+        { id: 1, area: 'dummy', section: 'dummy' },
+        { id: 2, area: 'dummy', section: 'dummy' },
+        { id: 3, area: 'dummy', section: 'dummy' },
+    ];
+    const renderRow = (row: TDummy) => {
+        return (
+            <TableRow key={row.id}>
+                <TableCell>{row.area}</TableCell>
+                <TableCell>{row.section}</TableCell>
+            </TableRow>
+        );
+    };
+    return (
+        <Stack>
+            <Title title="지역 및 섹션 목록" />
+            <PageLayout>
+                <Table headData={['지역명', '섹션명']} bodyData={dummy} renderRow={renderRow} />
+            </PageLayout>
+        </Stack>
+    );
 };
 
 export default LocationList;
--- src/pages/TreesPurchases/TreesPurchaseList/index.tsx
+++ src/pages/TreesPurchases/TreesPurchaseList/index.tsx
@@ -1,7 +1,81 @@
-import { Stack } from '@mui/material';
+import { Button, Stack, TableCell, TableRow } from '@mui/material';
+import dayjs from 'dayjs';
 
+import PageLayout from '@/components/PageLayout';
+import Table from '@/components/Table';
+import Title from '@/components/Title';
+
+interface TDummy {
+    id: number;
+    manager: string;
+    purchaseDate: Date;
+    count: number;
+    price: number;
+    totalPrice: number;
+    minusPrice: number;
+    payment: number;
+}
 const TreesPurchaseList = () => {
-    return <Stack sx={{ width: '100%' }}>수매 목록</Stack>;
+    const dummy = [
+        {
+            id: 1,
+            manager: 'dummy',
+            purchaseDate: new Date(),
+            count: 5,
+            price: 51000,
+            totalPrice: 3100,
+            minusPrice: 3000,
+            payment: 100,
+        },
+        {
+            id: 2,
+            manager: 'dummy',
+            purchaseDate: new Date(),
+            count: 5,
+            price: 51000,
+            totalPrice: 3100,
+            minusPrice: 3000,
+            payment: 100,
+        },
+        {
+            id: 3,
+            manager: 'dummy',
+            purchaseDate: new Date(),
+            count: 5,
+            price: 51000,
+            totalPrice: 3100,
+            minusPrice: 3000,
+            payment: 100,
+        },
+    ];
+
+    const renderRow = (row: TDummy) => {
+        return (
+            <TableRow key={row.id}>
+                <TableCell>{row.manager}</TableCell>
+                <TableCell>{dayjs(row.purchaseDate).format('YYYY-MM-DD')}</TableCell>
+                <TableCell>{row.count}</TableCell>
+                <TableCell>{row.price}</TableCell>
+                <TableCell>{row.totalPrice}</TableCell>
+                <TableCell>{row.minusPrice}</TableCell>
+                <TableCell>{row.payment}</TableCell>
+            </TableRow>
+        );
+    };
+    return (
+        <Stack>
+            <Title title="수매 목록">
+                <Button variant="containedBlue">수매 내역 등록</Button>
+            </Title>
+            <PageLayout>
+                <Table
+                    headData={['담당자', '거래일자', '수량', '단가', '총액', '차감액', '지급액']}
+                    bodyData={dummy}
+                    renderRow={renderRow}
+                />
+            </PageLayout>
+        </Stack>
+    );
 };
 
 export default TreesPurchaseList;
