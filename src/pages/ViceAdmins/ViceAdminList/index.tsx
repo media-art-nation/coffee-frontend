0311002be7b6ada6aa5e75df2f91077a131e0d4a
eunhee <6810779s@naver.com>
2025-04-21 16:41:56 +0900

    [feat] 목록 관련 레이아웃 화면 구현 완료

--- src/pages/ViceAdmins/ViceAdminList/index.tsx
+++ src/pages/ViceAdmins/ViceAdminList/index.tsx
@@ -1,6 +1,45 @@
-import { Stack } from '@mui/material';
+import { useNavigate } from 'react-router';
+
+import { Stack, TableCell, TableRow } from '@mui/material';
+
+import PageLayout from '@/components/PageLayout';
+import Table from '@/components/Table';
+import Title from '@/components/Title';
+
+interface TDummy {
+    id: number;
+    name: string;
+    userId: string;
+    managingArea: string;
+}
 
 const ViceAdminList = () => {
-    return <Stack sx={{ width: '100%' }}>부 관리자 목록</Stack>;
+    const dummy: TDummy[] = [
+        { id: 1, name: 'dummy', userId: 'dummy', managingArea: '경기' },
+        { id: 2, name: 'dummy', userId: 'dummy', managingArea: '경기' },
+    ];
+    const navigate = useNavigate();
+
+    const renderRow = (row: TDummy) => {
+        return (
+            <TableRow key={row.id} onClick={() => navigate(`/vice-admins/${row.id}`)}>
+                <TableCell>{row.name}</TableCell>
+                <TableCell>{row.userId}</TableCell>
+                <TableCell>{row.managingArea}</TableCell>
+            </TableRow>
+        );
+    };
+    return (
+        <Stack>
+            <Title title={'부 관리자 목록'} />
+            <PageLayout>
+                <Table
+                    headData={['이름', '아이디', '관리 지역']}
+                    bodyData={dummy}
+                    renderRow={renderRow}
+                />
+            </PageLayout>
+        </Stack>
+    );
 };
 export default ViceAdminList;
