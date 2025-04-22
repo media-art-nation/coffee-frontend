c6662f401cb6c7bb73af145e89e2c674aabf2897
eunhee <6810779s@naver.com>
2025-04-22 14:49:14 +0900

    [feat] 지역 생성, 섹션 생성 페이지 레이아웃 구현 완료

--- src/pages/Locations/LocationRegister/index.tsx
+++ src/pages/Locations/LocationRegister/index.tsx
@@ -1,7 +1,34 @@
-import { Stack } from '@mui/material';
+import { useForm } from 'react-hook-form';
 
+import { Button, Stack } from '@mui/material';
+
+import LabelComponentsLayout from '@/components/LabelComponentsLayout';
+import PageLayout from '@/components/PageLayout';
+import SearchTextField from '@/components/SearchTextField';
+import Title from '@/components/Title';
+
+interface TLocationInput {
+    area: string;
+}
 const LocationRegister = () => {
-    return <Stack sx={{ width: '100%' }}>지역 생성</Stack>;
+    const methods = useForm<TLocationInput>();
+    return (
+        <Stack>
+            <Title title="지역 생성">
+                <Button variant="containedGrey">취소</Button>
+                <Button variant="containedBlue">등록</Button>
+            </Title>
+            <PageLayout>
+                <LabelComponentsLayout labelValue="지역 검색">
+                    <SearchTextField
+                        fieldName="area"
+                        register={methods.register}
+                        placeholder="지역을 검색해주세요."
+                    />
+                </LabelComponentsLayout>
+            </PageLayout>
+        </Stack>
+    );
 };
 
 export default LocationRegister;
--- src/pages/Locations/SectionRegister/index.tsx
+++ src/pages/Locations/SectionRegister/index.tsx
@@ -1,7 +1,46 @@
-import { Stack } from '@mui/material';
+import { useForm } from 'react-hook-form';
 
+import { Button, Stack } from '@mui/material';
+
+import LabelAndSelect from '@/components/LabelAndSelect';
+import LabelComponentsLayout from '@/components/LabelComponentsLayout';
+import PageLayout from '@/components/PageLayout';
+import SearchTextField from '@/components/SearchTextField';
+import Title from '@/components/Title';
+
+interface TSectionInput {
+    area: string;
+    section: string;
+}
 const SectionRegister = () => {
-    return <Stack sx={{ width: '100%' }}>섹션 생성</Stack>;
+    const methods = useForm<TSectionInput>();
+    return (
+        <Stack>
+            <Title title="섹션 생성">
+                <Button variant="containedGrey">취소</Button>
+                <Button variant="containedBlue">등록</Button>
+            </Title>
+            <PageLayout gap={'27px'}>
+                <LabelAndSelect
+                    labelValue="지역"
+                    control={methods.control}
+                    selectArr={[
+                        { value: '1', label: '경기도' },
+                        { value: '2', label: '부산' },
+                    ]}
+                    fieldName="area"
+                    placeholder="지역을 선택해주세요"
+                />
+                <LabelComponentsLayout labelValue="섹션 검색">
+                    <SearchTextField
+                        fieldName="area"
+                        register={methods.register}
+                        placeholder="지역을 검색해주세요."
+                    />
+                </LabelComponentsLayout>
+            </PageLayout>
+        </Stack>
+    );
 };
 
 export default SectionRegister;
