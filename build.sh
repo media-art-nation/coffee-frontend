4ce3c1b4e730f1d7e3d808c773da56b744f02c32
yuna872 <yunae426@gmail.com>
2025-06-21 21:52:28 +0900

    [chore] build sh 수정

--- build.sh
+++ build.sh
@@ -2,17 +2,13 @@
 
 echo "🛠 Vite 프로젝트 빌드 시작"
 
-# 1. output 폴더 초기화
 rm -rf output
 mkdir output
 
-# 2. 의존성 설치
 yarn install --frozen-lockfile
-
-# 3. 빌드 실행
 yarn build
 
-# 4. 빌드 결과 전체 복사
 cp -r dist/. output/
+cp vercel.json output/
 
 echo "✅ 빌드 완료 - output 폴더 생성됨"
